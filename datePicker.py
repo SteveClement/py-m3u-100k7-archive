300e90093428df70dd877f69387c2de03ebf68f3
Steve Clement <steve@localhost.lu>
2015-06-10 10:58:49 +0200

    optionList

--- datePicker.py
+++ datePicker.py
@@ -5,6 +5,9 @@ from wtforms.fields.html5 import DateField
 app = Flask(__name__)
 app.secret_key = 'coa7674c694607b169e57593a4952ea26fol!'
 
+shows = [ "Mënsch an Ëmwelt","Noriichten","Klassik oder Klassësch","Schwéierpunkt Klassik","Dageschronik","Klassikthema","Sequenza","Prisma","Moies Panorama","Mëttes Panorama","Owes Panorama","Europa an Internationales","Schwéierpunkt Théater","De Komponist vum Mount","Presserevue","Invité vum Dag","Dossier vum Dag","Bits & Bytes","Mixtape","Kultur Thema","Kultur am Gespréisch","Voices","Startup","Liewensformen","Schwéierpunkt Konscht","Radio Zoo","Wirtschafts Wëssen","Wëssenschaft a Fuerschung","Ee Mount een Thema: Welt am ëmbroch","Ee Mount een Thema: Generic","Spezial Emissioun: Referendum","Spezial Emissioun: Generic","Cool Britannia" ]
+
+
 class Form(Form):
     dt = DateField('DatePicker', format='%Y-%m-%d')
 
@@ -14,7 +17,7 @@ def hello_world():
     form = Form()
     if form.validate_on_submit():
         return form.dt.data.strftime('%Y-%m-%d')
-    return render_template('audioArchiv.html', form=form)
+    return render_template('audioArchiv.html', form=form,show=shows)
 
 
 if __name__ == '__main__':
